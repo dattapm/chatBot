dcc5157c5cfc15ed307fa84c37f5ab4cabc4b2c2
Datta Maddikunta <datta.pm@gmail.com>
2018-01-12 01:34:31 -0800

    Add support for JSON response

--- opt/lessenger/chatbot/wsgi-bin/hello.py
+++ opt/lessenger/chatbot/wsgi-bin/hello.py
@@ -1,28 +1,57 @@
+from string import Template
+
 class ChatBot(object):
 
   def __init__(self):
-    print "in init"
+    print "CHATBOT IS ENABLED"
+
+    self.json_response = """
+        {
+             "messages": [
+                 ${MESSAGES_ARRAY}
+             ]
+        }\
+       """
+
+    self.rsp_msg = """
+        {
+              "type": "${TYPE}",
+              "${FORMAT}": "${OUTPUT_MESSAGE}"
+        }\
+       """
+
+    self.json_response_template = Template(self.json_response)
+    self.rsp_msg_template = Template(self.rsp_msg)
+
 
   def __call__(self, environ, start_response):
      status = '200 OK'
      output = 'Hello World!'
 
      response_headers = [
-      ('Access-Control-Allow-Origin', '*'),
-      ('Access-Control-Allow-Headers', 'Content-Type'),
-      ('Access-Control-Allow-Methods', 'GET, POST'),
-      ('Content-Type', 'text/plain'),
-      ('Content-Length', str(len(output)))
-     ]
+           ('Access-Control-Allow-Origin', '*'),
+           ('Access-Control-Allow-Headers', 'Content-Type'),
+           ('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'),
+           ('Content-Type', 'text/plain'),
+         ]
+
+     rsp_msg_local = self.rsp_msg_template.substitute(TYPE="text",
+                                             FORMAT="text",
+                                             OUTPUT_MESSAGE=output)
+
+     json_rsp_local = self.json_response_template.substitute(MESSAGES_ARRAY=rsp_msg_local)
+
+     response_headers.append(('Content-Length',str(len(json_rsp_local))))
 
      start_response(status, response_headers)
 
-     return [output]
+     return [json_rsp_local]
 
   def __del__(self):
-    print "in del"
+    print "CHATBOT IS DISABLED"
 
 def main():
+  application = ChatBot() 
   application()
 
 if __name__ == "__main__":
