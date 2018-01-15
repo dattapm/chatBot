1e5b7ad3160d3ae40b4d0a4c13b699a0054a8471
Datta Prabhu Maddikunta <datta.pm@gmail.com>
2018-01-14 17:53:11 -0800

    Add support to read the POST form data.
    Add support to read the POST form data from the Lessenger UI.

--- opt/lessenger/chatbot/chatbot.py
+++ opt/lessenger/chatbot/chatbot.py
@@ -1,4 +1,9 @@
 from string import Template
+import urllib2
+import json
+import cgi
+from cStringIO import StringIO
+#from cgi import parse_qs, escape
 
 class ChatBot(object):
 
@@ -24,6 +29,32 @@ class ChatBot(object):
     self.rsp_msg_template = Template(self.rsp_msg)
 
 
+  def GetFormData(self, environ):
+    # When the method is POST the variable will be sent
+    # in the HTTP request body which is passed by the WSGI server
+    # in the file like wsgi.input environment variable.
+
+    content_type = environ['CONTENT_TYPE']
+    if content_type:
+      content_type_value, content_type_dict = cgi.parse_header(content_type)
+      if content_type_value == 'multipart/form-data':
+        fp = environ['wsgi.input']
+        user_input_dict = cgi.parse_multipart(environ['wsgi.input'], content_type_dict)
+
+    return user_input_dict
+
+  def ProcessClientRequest(self, environ):
+
+    user_input_dict = self.GetFormData(environ)
+
+    print user_input_dict.get("action")
+    # Based on the action type, call the apropriate API
+    if user_input_dict.get("action")[0] == "join":
+       # Call the /Welcome API
+       print "IN WELCOME API"
+    elif user_input_dict.get("action")[0] == "message":
+       print "IN MESSAGE API"
+
   def __call__(self, environ, start_response):
      status = '200 OK'
      output = 'Hello World!'
@@ -34,6 +65,11 @@ class ChatBot(object):
            ('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'),
            ('Content-Type', 'text/plain'),
          ]
+
+
+
+     # Retrieve user input from POST request.
+     output_new = self.ProcessClientRequest(environ)
 
      rsp_msg_local = self.rsp_msg_template.substitute(TYPE="text",
                                              FORMAT="text",
